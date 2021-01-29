87411178822edace3c6f9368cf9b85ac2d46d5eb
Adam Slaker <adamslaker@gmail.com>
2021-01-29 13:47:50 -0600

    Update dependencies

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export const api = axios.create({
+export const githubAPI = axios.create({
   baseURL: "https://api.github.com",
+});
+
+export const voteAPI = axios.create({
+  baseURL: ".netlify/functions",
 });
